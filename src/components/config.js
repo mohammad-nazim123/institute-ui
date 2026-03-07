0bfad3490acf34271d594fa8759a21fb538fe7a1
nazim-influencer <mohammad.nazim@influencer.in>
2026-03-07 19:30:10 +0530

    created API_URL

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
-// const API_URL = "http://localhost:8000";
-// const API_URL = "http://[IP_ADDRESS]";
-const API_URL = process.env.REACT_APP_API_HOST || "http://localhost:8000";
+
+// const API_URL = process.env.REACT_APP_API_HOST || "http://localhost:8000";
+const API_URL = "http://43.205.116.183:8000" || "http://localhost:8000";
 
 export default API_URL;
