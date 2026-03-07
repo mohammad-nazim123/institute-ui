bede08cdf68ab278dc07f9e8ee60c2f11c0b33ae
nazim-influencer <mohammad.nazim@influencer.in>
2026-03-07 23:35:26 +0530

    abcd

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 
 // const API_URL = process.env.REACT_APP_API_HOST || "http://localhost:8000";
-const API_URL = "http://13.234.238.109:8000" || "http://localhost:8000";
+const API_URL = "https://main.d3e0ptm41k0biu.amplifyapp.com" || "http://localhost:8000";
 
 export default API_URL;
