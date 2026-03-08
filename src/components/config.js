f027e053e409247dc8b66e91fe5c69c284aa5ecb
nazim-influencer <mohammad.nazim@influencer.in>
2026-03-08 14:15:47 +0530

    add slash

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 
 // const API_URL = process.env.REACT_APP_API_HOST || "http://localhost:8000";
-const API_URL = "https://vrevk5vzamhsllkypsfme4y2p40uejdg.lambda-url.ap-southeast-2.on.aws" || "http://localhost:8000";
+const API_URL = "https://vrevk5vzamhsllkypsfme4y2p40uejdg.lambda-url.ap-southeast-2.on.aws/" || "http://localhost:8000";
 
 export default API_URL;
